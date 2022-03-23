7b33a3201099a5761ba361af6f6ccff61ae5b7f4
Austin Spencer <aspencer527@gmail.com>
2022-03-22 20:55:00 -0400

    Initail stuff I think

---
+++ src/main/java/frc/robot/commands/Auto/AutoHang1.java
@@ -0,0 +1,19 @@
+package frc.robot.commands.Auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.HangLeft;
+import frc.robot.subsystems.HangRight;
+
+public class AutoHang1 extends SequentialCommandGroup  {
+
+    public AutoHang1(HangLeft left,HangRight right){
+        addCommands(
+            new SequentialCommandGroup(
+                //new 
+
+                
+            )
+        );
+        
+    }
+}
