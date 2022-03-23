199be789e34350fa479f99973d7d0cfc1ab50c61
Austin Spencer <aspencer527@gmail.com>
2022-03-22 20:11:39 -0400

    did more things

--- main/java/frc/robot/commands/Hang/StopLeft.java
+++ main/java/frc/robot/commands/Hang/StopLeft.java
@@ -1,3 +1,24 @@
-public class StopLeft {
-    
+import frc.robot.subsystems.HangLeft;
+
+//package frc.robot.commands.Hang;
+
+public class StopLeft extends CommandBase {
+    HangSubsystem m_hangSubsystem;
+    boolean state;
+
+    public StopLeft(HangLeft hang, boolean state) {
+        m_hangSubsystem = hang;
+        this.state=state;
+    }
+
+    @Override
+    public void initialize() {
+        m_hangSubsystem.leftHangStop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        
+        return true;
+    }
 }
--- main/java/frc/robot/commands/Hang/StopRight.java
+++ main/java/frc/robot/commands/Hang/StopRight.java
@@ -1,3 +1,23 @@
-public class StopRight {
-    
+import frc.robot.subsystems.HangLeft;
+
+//package frc.robot.commands.Hang;
+
+public class StopRight extends CommandBase {
+    HangSubsystem m_hangSubsystem;
+    boolean state;
+
+    public StopRight(HangLeft hang, boolean state) {
+        m_hangSubsystem = hang;
+        this.state = state;
+    }
+
+    @Override
+    public void initialize() {
+        m_hangSubsystem.rightHangStop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
 }
